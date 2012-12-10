9cc82398ae48137f3f44ac3ce915fe34465ee67a
Ian Wilson <ian.owings@gmail.com>
2012-12-10 11:29:44 -0500

    Fixed up form imports for model fields.

--- django_localflavor_us/models.py
+++ django_localflavor_us/models.py
@@ -2,6 +2,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.db.models.fields import CharField
 from django.contrib.localflavor.us.us_states import STATE_CHOICES
 from django.contrib.localflavor.us.us_states import USPS_CHOICES
+from django.contrib.localflavor.us import forms
 
 class USStateField(CharField):
 
@@ -30,8 +31,7 @@ class PhoneNumberField(CharField):
         super(PhoneNumberField, self).__init__(*args, **kwargs)
 
     def formfield(self, **kwargs):
-        from django.contrib.localflavor.us.forms import USPhoneNumberField
-        defaults = {'form_class': USPhoneNumberField}
+        defaults = {'form_class': forms.USPhoneNumberField}
         defaults.update(kwargs)
         return super(PhoneNumberField, self).formfield(**defaults)
 
@@ -41,13 +41,12 @@ class USZipCodeField(CharField):
 
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = 10
-        super(ZipCodeField, self).__init__(*args, **kwargs)
+        super(USZipCodeField, self).__init__(*args, **kwargs)
 
     def formfield(self, **kwargs):
-        from django.contrib.localflavor.us.forms import USZipCodeField
-        defaults = {'form_class': USZipCodeField}
+        defaults = {'form_class': forms.USZipCodeField}
         defaults.update(kwargs)
-        return super(ZipCodeField, self).formfield(**defaults)
+        return super(USZipCodeField, self).formfield(**defaults)
     
 
 # gracefully fail south introspection if south is not available
@@ -57,7 +56,7 @@ try:
     add_introspection_rules([], ["^django_localflavor_us\.models\.USStateField"])
     add_introspection_rules([], ["^django_localflavor_us\.models\.USPostalCodeField"])
     add_introspection_rules([], ["^django_localflavor_us\.models\.PhoneNumberField"])
-    add_introspection_rules([], ["^django_localflavor_us\.models\.ZipCodeField"])
+    add_introspection_rules([], ["^django_localflavor_us\.models\.USZipCodeField"])
 except ImportError:
     pass
 
